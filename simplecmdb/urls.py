7607cbfcc70cd901c435f5f0c9222acbfabcf629
Sibiao Luo <sibiaoluo@gmail.com>
2013-08-02 18:25:02 +0800

    modify the urls adding a identify item

--- simplecmdb/urls.py
+++ simplecmdb/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^api/collect$', 'hostinfo.views.collect'),
+    url(r'^api/gethostbyident$', 'hostinfo.views.getHostByIdentity'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/gethosts\.json$', 'hostinfo.views.gethostsjson'),
     url(r'^api/gethosts\.txt$', 'hostinfo.views.gethoststxt'),
